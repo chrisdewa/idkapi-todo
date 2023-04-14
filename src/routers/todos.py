3a45721d34cb5dbb15717fe275204f32117709f2
ChrisDewa <christian.delaflor@gmail.com>
2023-04-14 09:16:32 -0600

    fixed todos/all

--- src/routers/todos.py
+++ src/routers/todos.py
@@ -3,14 +3,15 @@ from fastapi import APIRouter, Depends
 from .users import UserDeps
 
 from src.db.models import Todo
-from src.db.schemas import TodoOutSet
+from src.db.schemas import TodoOut
 
-router = APIRouter(prefix='/todos')
+router = APIRouter(prefix='/todos', tags=['todos'])
 
 
 
 @router.get('/all')
-async def get_user_todos(user: UserDeps) -> TodoOutSet:
+async def get_user_todos(user: UserDeps) -> list[TodoOut]:
     """Returns the user's todos"""
     todos = await user.todos
-    return TodoOutSet.from_orm(todos)
+    return [TodoOut.from_orm(todo) for todo in todos]
+    
