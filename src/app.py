29430ed5c1c506c0fea3e2ee3d01ec04a873e486
ChrisDewa <christian.delaflor@gmail.com>
2023-04-14 09:10:56 -0600

    added todo router, better naming in folders

--- src/app.py
+++ src/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
-from src.routes.users import router as user_router
+from src.routers.users import router as user_router
+from src.routers.todos import router as todo_router
 
 app = FastAPI(docs_url='/')
 
 app.include_router(user_router)
+app.include_router(todo_router)
 
 @app.get('/health')
 async def health_check() -> str:
